1a794c892553bab17fd6abf333b6584bf5a6e34a
Lucasvbastos <lucasvianabastos@gmail.com>
2023-03-07 15:04:23 -0300

    New commit

---
+++ style.js
@@ -0,0 +1,13 @@
+Function verif() {
+    let a1 = document.getElementById('num1')
+    let a2 = document.getElementById('num2')
+    let n1 = Number(a1.value)
+    let n2 = Number(a2.value)
+    let v = n2 > n1
+
+    if v === true
+    res.innerHTML = `O valor de B(<strong>${n2}</strong>) é maior que o valor de A(<strong>${n2}</strong>)`
+
+    else res.innerHTML = `O valor de B(<strong>${n2}</strong>) é maior que o valor de A(<strong>${n2}</strong>)`
+
+}
