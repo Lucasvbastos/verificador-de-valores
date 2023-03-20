ea5cff471b6da60ac5a9dc581f38145c1185bed6
Lucas Viana Bastos <117181689+Lucasvbastos@users.noreply.github.com>
2023-03-20 20:18:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 function verif() {
   let A = document.querySelector(`input#A`);
   let B = document.querySelector(`input#B`);
-  let res = document.querySelector(`div#res`)
   let valA = Number(A.value);
   let valB = Number(B.value);
+  const mensagemSucesso ='Parabéns, formulário válido!'
+
   
-  if (valB > valA) {
-    //alert("Parabéns, formulário válido!");
-    res.innerText = "Parabéns, formulário válido!"
+  if (valB >= valA) {
+    const containerMensagemSucesso = document.querySelector('.success-message');
+    document.querySelector('.success-message').innerHTML = mensagemSucesso;
+    containerMensagemSucesso.style.display = 'block';
     return true;
   } else {
-    //alert("Valor de B deve ser maior que A.");
-    res.innerHTML = "Valor de B deve ser maior que A."
+    document.querySelector('.error-message').style.display= 'block';
     return false;
 }}
